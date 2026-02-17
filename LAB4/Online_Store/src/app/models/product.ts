002d9245eb0cfecf498d20523b01290791fcc636
AmirZholdasali <zoldasaliamir@gmail.com>
2026-02-17 10:11:46 +0500

    Add Lab4 online-store

---
+++ LAB4/Online_Store/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  rating: number;
+  image: string;
+  link: string; // ссылка на товар в Kaspi
+  price: number;
+}
